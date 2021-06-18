c350b34d5c8e71dbf553abf7481d53cc93dd4e8e
louazz <louaizaiter@gmail.com>
2021-06-18 23:00:13 +0100

    change

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
+/**
+ This code is made by Louai Zaiter 
+**/
+public class AppTest {
+    public static void main(String[] args)
+    {
+        //System.out.println(KycGet("url","key"));
+        //System.out.println(kycAdd("url", "body","apikey").toString());
+    }
+
+    public String KycGet(String url, String apikey) {
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(url).header("accept", "application/json")
+                .queryString("apikey", apikey).asJson();
+
+        System.out.println(jsonResponse.getBody());
+        System.out.println(jsonResponse.getStatus());
+        return jsonResponse.getBody().toString();
+    }
+
+    public int KycAdd(String url, String body, String apikey) {
+
+        HttpResponse<String> stringResponse = Unirest.post(url)
+                .header("accept", "application/json").queryString("apikey", apikey)
+                .body(body).asString();
+
+        return stringResponse.getStatus();
+    }
+}
